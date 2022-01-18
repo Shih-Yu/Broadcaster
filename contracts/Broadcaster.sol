358e6f6a3af1dea43866fee58eeec6c5aa03f04c
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-01-18 16:33:28 -0600

    add Broadcaster contract

--- contracts/Broadcaster.sol
+++ contracts/Broadcaster.sol
@@ -6,3 +6,30 @@ pragma solidity ^0.8.6;
     *Use Chainlink to display current price of currencies interface
     *
 **/
+
+interface PriceFeed {
+  function getLatestPrice() external view returns(uint);
+}
+
+contract Broadcaster {
+  // Assign deployer the owner of the contract and can receive payment
+  address payable public owner;
+  // Balance of this contract
+  uint public balance;
+
+
+constructor() {
+    owner = payable (msg.sender);
+}
+
+// Function for tipping the broadcaster
+function tip() public payable {
+  // Checking that person tipping is not the owner
+  require(msg.sender != owner, "Owner cannot tip themselves");
+  // Send tip  to the contract
+  payable (msg.sender).transfer(msg.value);
+  // Updates balnce of the contract
+  balance += msg.value;
+}
+
+}
